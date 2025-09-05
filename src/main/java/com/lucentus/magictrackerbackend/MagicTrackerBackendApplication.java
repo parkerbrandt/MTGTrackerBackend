614bf09ff800834c4407e91ac5e7a4f98abe66b7
Parker Brandt <parker.brandt101@gmail.com>
2025-09-05 10:40:47 -0500

    Initial commit

---
+++ src/main/java/com/lucentus/magictrackerbackend/MagicTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lucentus.magictrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicTrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicTrackerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucentus/magictrackerbackend/MagicTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucentus.magictrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicTrackerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
